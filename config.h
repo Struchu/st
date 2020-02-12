b9cf6cfa5464866bc515bd5738a40466addc8dd5
Marcin Struś <marcin.strus@makimo.pl>
2020-02-12 08:01:33 +0100

    Add externalpipe to open the given URL

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Menlo for Powerline:pixelsize=13:antialias=true:autohint=true";
+static char *font = "Inconsolata:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -152,6 +152,12 @@ static MouseShortcut mshortcuts[] = {
 	{ Button5,              XK_ANY_MOD,     "\005" },
 };
 
+/*
+ * Extra commands.
+ */
+static char *openurlcmd[] = { "/bin/sh", "-c",
+    "xurls | dmenu -l 10 -i -w $WINDOWID -fn 'inconsolata:size=13' -nb '#fdf6e3' -nf '#657b83' -sb '#eee8d5' -sf '#657b83' | xargs url_handler.sh", NULL };
+
 /* Internal keyboard shortcuts. */
 #define MODKEY Mod1Mask
 #define TERMMOD (ControlMask|ShiftMask)
@@ -170,6 +176,7 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ TERMMOD,              XK_U,           externalpipe,   {.v =  openurlcmd } },
 };
 
 /*
